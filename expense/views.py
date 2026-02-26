e02fbe2f60be0f56bed816b8845b59fa4ba2590e
Hill Soni Dev80 <pragnakalp.dev80@gmail.com>
2026-02-26 22:47:03 +0530

    register and login

---
+++ expense/views.py
@@ -0,0 +1,55 @@
+from django.contrib import messages
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404,redirect
+from django.views import generic, View
+from .forms import CustomUserCreationForm, LoginForm
+
+# Create your models here.
+class RegistrationView(View):
+    def get(self, request):
+        # if request.user.is_authenticated:
+        #     return redirect("/expense/dashboard")
+        form = CustomUserCreationForm()
+        return render(request, 'expense/register.html', { 'form': form})  
+    
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/expense/login/')        
+        messages.info(request, "Account created Successfully!")
+        
+class LoginView(LoginView):
+    template_name = 'expense/login.html'
+    def get(self, request):
+        form= AuthenticationForm()
+        return render(request, self.template_name, { 'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = authenticate(request, username=email, password=password)
+            print(user)
+            if user is None:
+                messages.error(request,"Email or Password not matched")
+                return redirect('/expense/login/')
+            login(request,user)
+            return redirect("/expense/dashboard")
+        else:
+            messages.error(request, "SOmething is wrong!")
+
+class LogoutView(View):
+    def post(self,request):
+        if request.user.is_authenticated:
+            logout(request)
+            return redirect('/expense/login/')
+
+class DashboardView(View):
+    template_name = 'expense/dashboard.html'
+    def get(self, request):
+        return render(request, self.template_name)
